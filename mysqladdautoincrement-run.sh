887f797f5078d03f6eb54c986f0ca0e886651156
developer961@gmail.com <abasi@radargps.ir>
2020-09-07 14:16:54 +0430

    devops-3-Save key_columns,tables

--- mysqladdautoincrement-run.sh
+++ mysqladdautoincrement-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 while read line;
 do
+line=$(echo -e $line | sed 's/^[" \t]*//;s/[" \t]*$//')
+#echo $line
 . ./mysqladdautoincrement.sh $line;
 echo "--------------------------";
 done < ./cms/tables-all.csv
